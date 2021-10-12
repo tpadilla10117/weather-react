0beef882b6a6a1c87c77235b6be2723d42918153
Trinidad Padilla <tpadilla101@live.com>
2021-10-12 14:27:41 -0700

    created state to dynamically render data and passed in components as props

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { HomePage} from './utils';
 
 require('dotenv').config();
@@ -7,10 +7,27 @@ const apiKey = process.env.WEATHER_KEY;
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 function App() {
+
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+/* To dynamically render weather data: */
+  const search = event => {
+    if(event.key === "Enter") {
+      fetch(`${BASE_URL}weather?q=${query}&units=metric&APPID=${apiKey}`)
+        .then(res => res.json() )
+        .then(result => setWeather(result) );
+    }
+  };
+
+
   return (
     <div className="App">
 
-      <HomePage />
+      <HomePage query={query} setQuery={setQuery} weather={weather} setWeather={setWeather} search={search}
+        apiKey={apiKey}
+        BASE_URL={BASE_URL}
+      />
 
     </div>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 import { SearchBox, LocationBox, WeatherBox } from '../../components/utils';
 import './Home.css';
 
-const HomePage = () => {
+const HomePage = (props) => {
+
+    const {query, setQuery, weather, setWeather, search, apiKey, BASE_URL} = props;
+
     return (
         <main>
             <SearchBox />
